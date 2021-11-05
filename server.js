d42fdf8224fe0b6d54194a38e293be491d96d8a8
mallmann02 <leomallmann020302@gmail.com>
2021-11-05 16:08:47 -0300

    feat: config cors

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const bodyparser = require('body-parser');
+const cors = require('cors');
 const TaskRouter = require('./Routes/tasksRouter');
 
 const app = express();
 
+app.use(cors({
+  origin: 'http://localhost:3000'
+}));
 app.use(bodyparser.json());
 
 app.use('/tasks', TaskRouter);
